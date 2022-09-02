7aab3f7142bf3d3df9994b01f82d64197aea4dbe
zixuanwang342 <zixuanwang342@gmail.com>
2022-09-02 22:00:25 +0100

    Update
    Update

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,14 +12,15 @@ urlpatterns = [
     # path for about view
     path(route='about', view=views.about, name='about'),
     # path for contact us view
-path(route='contact', view=views.contact, name='contact'),
+    path(route='contact', view=views.contact, name='contact'),
     # path for registration
-
+path('registration/', views.registration_request, name='registration'),
     # path for login
-
+    path(route='login', view=views.login_request, name='login'),
     # path for logout
-
-    path(route='', view=views.get_dealerships, name='index'),
+    path(route='logout', view=views.logout_request, name='logout'),
+    #path(route='', view=views.get_dealerships, name='index'),
+path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
 
